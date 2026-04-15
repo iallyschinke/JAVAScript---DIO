636a2cfbe66cd9599f26897a082e3273684e8df0
IallySchinke <iallyschinke@gmail.com>
2026-04-15 18:57:16 -0300

    2 - estruturas condicionais #1 commit

---
+++ Variáveis e Operadores/primeiro_desafio.js
@@ -0,0 +1,19 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+    1 - preço do combustível;
+    2 - Gasto médio de combustível do carro por KM;
+    3 - Distância em KM da viagem;
+
+
+*/
+
+const precoCombustivel = 5.79;
+const kmPorLitro = 12;
+const distanciaEmKm = 1580;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitro;
+
+const ValorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(ValorGasto.toFixed(2));
